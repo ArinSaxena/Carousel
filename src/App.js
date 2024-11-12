feacb2e775c1ab0ffecf2d71665c15314893c8c2
Arin Saxena <arinsaxena0002@gamil.com>
2024-11-12 23:34:34 +0530

    crousel complete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import slides from "data/data.js";
 import Carousel from "components/Carousel/Carousel.jsx";
 function App() {
   return (
-    <div className="App" >
+    <div className="App" style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh", width:"100vw"}} >
       <Carousel slides={slides}/>
     </div>
   );
