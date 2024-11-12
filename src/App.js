603b419ba5942454060cb22ffe83977a802d3453
Arin Saxena <arinsaxena0002@gamil.com>
2024-11-12 23:32:06 +0530

    crousel complete

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Carousel from "components/Carousel/Carousel.jsx";
 function App() {
   return (
     <div className="App" >
-      <h1>Hello carousel</h1>
       <Carousel slides={slides}/>
     </div>
   );
